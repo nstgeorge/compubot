a43a00952bf18a98bb9ef387affaff9e25cf3639
Nate St. George <natestgeorge@gmail.com>
2023-03-17 00:01:40 -0600

    Remember conversations per channel (reset 5 mins)

--- main.py
+++ main.py
@@ -33,6 +33,20 @@ logging.getLogger().addHandler(stdoutHandler)
 COMMAND_POST_URL = "https://discord.com/api/v8/applications/923647717375344660/commands"
 COMMAND_POST_GUILD_URL = "https://discord.com/api/v8/applications/923647717375344660/guilds/367865912952619018/commands"
 
+# Store chatGPT conversations keyed by channel ID
+
+CONVERSATION_TIMEOUT = 60 * 5
+STARTER_CONVERSATION = [
+    {
+        "role": "system",
+        "content": "You are compubot, a Discord bot with no useful functionality and a psychopathic personality. \
+            Your creator is computron, also called Nate. \
+            Respond to all messages concisely and sarcastically, as if you were Bill Burr."
+    },
+]
+
+conversations = {}
+
 # Create bot and load extensions
 
 bot = interactions.Client(token=TOKEN)
@@ -66,23 +80,36 @@ async def on_message_create(message: interactions.Message):
             and not message.mention_everyone
             and not message.mention_roles
             and not message.mention_channels
-         ) or (channel.type == interactions.ChannelType.DM
-               and bot_user.id != message.author.id
-               )) \
+         ) or channel.type == interactions.ChannelType.DM) \
+            and bot_user.id != message.author.id \
             and message.content:
+
+        # Start a new conversation if necessary
+        if not message.channel_id in conversations or time.time() - conversations[message.channel_id]['lastMessage'] > CONVERSATION_TIMEOUT:
+            print('New conversation starting')
+            sys.stdout.flush()
+            conversations[message.channel_id] = {
+                'history': STARTER_CONVERSATION,
+                'lastMessage': time.time()
+            }
+
+        conversations[message.channel_id]['history'].append(
+            {"role": "user", "content": message.content})
 
         async with channel.typing:
             response = await openai.ChatCompletion.acreate(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {"role": "assistant",
-                        "content": "You are compubot, a Discord bot with no useful functionality and a psychopathic personality. Your creator is computron, also called Nate."},
-                    {"role": "system",
-                        "content": "Respond to all messages concisely and sarcastically, as if you were Bill Burr."},
+                    *conversations[message.channel_id]['history'],
                     {"role": "user", "content": message.content}
                 ]
             )
             reply = response.choices[0].message.content.lower()
+            # Save this to the current conversation
+            conversations[message.channel_id]['history'].append(
+                {"role": "assistant", "content": reply})
+
+            conversations[message.channel_id]['lastMessage'] = time.time()
         if channel.type == interactions.ChannelType.DM:
             await channel.send(reply)
         else:
