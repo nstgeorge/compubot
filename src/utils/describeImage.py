13c69a2df6371c3c409698f4bd5dbe221683ee7f
Nate St. George <natestgeorge@gmail.com>
2024-05-05 17:30:08 -0600

    Add catch for images that are too large

--- src/utils/describeImage.py
+++ src/utils/describeImage.py
@@ -1,6 +1,6 @@
 import os
 
-from openai import AsyncOpenAI
+from openai import AsyncOpenAI, BadRequestError
 
 API_URL = "https://api.fireworks.ai/inference/v1/"
 MODEL = "accounts/fireworks/models/firellava-13b"
@@ -8,32 +8,35 @@ MODEL = "accounts/fireworks/models/firellava-13b"
 client = AsyncOpenAI(base_url=API_URL, api_key=os.getenv("FIREWORKS_API_KEY"))
 
 async def describe_image(url: str):
-  response = await client.chat.completions.create(
-			model=MODEL,
-			max_tokens=512,
-			top_p=1,
-			presence_penalty=0,
-			frequency_penalty=0.5,
-			temperature=0.6,
-			messages=[
-        {
-          "role": "user",
-          "content": [
-            {
-              "type": "text",
-              "text": "Describe this image."
-            },
-            {
-              "type": "image_url",
-              "image_url": {
-                "url": url
+  try:
+    response = await client.chat.completions.create(
+        model=MODEL,
+        max_tokens=512,
+        top_p=1,
+        presence_penalty=0,
+        frequency_penalty=0.5,
+        temperature=0.6,
+        messages=[
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": "Describe this image."
+              },
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": url
+                }
               }
-            }
-          ]
-        }
-      ]
-		)
-  
-  print("Image description: {}".format(response.choices[0].message.content))
-  
-  return response.choices[0].message.content
+            ]
+          }
+        ]
+      )
+    
+    print("Image description: {}".format(response.choices[0].message.content))
+    
+    return response.choices[0].message.content
+  except BadRequestError:
+    return "The image is too large (5MB max)."
